ea7ed656f611fda188d86c08b55ec0aee2be596c
MatejKastak <matej.kastak@gmail.com>
2022-02-02 21:40:56 +0100

    feat: Register fish_vi_colemak_key_bindings

--- functions/_pure_get_prompt_symbol.fish
+++ functions/_pure_get_prompt_symbol.fish
@@ -4,7 +4,7 @@ function _pure_get_prompt_symbol \
 
     set --local prompt_symbol $pure_symbol_prompt
     test "$pure_reverse_prompt_symbol_in_vimode" = true
-    and string match -rq "fish_(vi|hybrid)_key_bindings" $fish_key_bindings
+    and string match -rq "fish_(vi|vi_colemak|hybrid)_key_bindings" $fish_key_bindings
     and not contains "$fish_bind_mode" insert replace
     and set prompt_symbol $pure_symbol_reverse_prompt
 
